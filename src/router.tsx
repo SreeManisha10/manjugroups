87982986cbfbdb5018ae30aa911a18c898639fd1
Your Name <your_email@gmail.com>
2026-09-19 00:43:01 +0530

    pagesss

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: import.meta.env.VITE_BASE_PATH ?? "/",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
