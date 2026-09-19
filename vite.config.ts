096e4fc041ba02845136d5be747b59f24c61b26f
Your Name <your_email@gmail.com>
2026-09-19 13:26:36 +0530

    Finall

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,11 @@ export default defineConfig({
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from thisOk
     server: { entry: "server" },
+    spa: {
+      enabled: true,
+      prerender: {
+        outputPath: "/_shell.html",
+      },
+    },
   },
 });
